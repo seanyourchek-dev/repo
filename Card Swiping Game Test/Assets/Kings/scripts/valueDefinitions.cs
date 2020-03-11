f72a47e9760645ffe0138614fbc4e9423d2e2c44
seanyourchek-dev <60983592+seanyourchek-dev@users.noreply.github.com>
2020-03-11 11:19:00 -0700

    Putting Project on Git

---
+++ Card Swiping Game Test/Assets/Kings/scripts/valueDefinitions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class valueDefinitions : MonoBehaviour {
+
+	/*
+	 * Define all your values of the game here.
+	 * The value definitions are used in several scripts to 
+	 * show easy dropdown menues e.g. for conditions or value modifications.
+	 */
+
+	public enum values{
+		name,
+		surname,
+		gender,
+		title,
+		level,
+		strength,
+		agility,
+		intelligence,
+		charisma
+	}
+}
